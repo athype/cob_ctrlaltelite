b1d33e141b414752dcb483fcc6f209ce1b02e868
nguyentuannguyenan <nguyenan29072001@gmail.com>
2024-12-03 17:11:35 +0100

    update Sveltestrap

--- client/src/pages/Home.svelte
+++ client/src/pages/Home.svelte
@@ -9,20 +9,13 @@
         NavbarBrand, NavbarToggler, NavItem, NavLink
     } from '@sveltestrap/sveltestrap';
     import { Icon } from '@sveltestrap/sveltestrap';
-    import { Carousel, CarouselControl, CarouselItem } from '@sveltestrap/sveltestrap';
-    const items = [
-        'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa1d%20text%20%7B%20fill%3A%23555%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa1d%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22285.921875%22%20y%3D%22218.3%22%3EFirst%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-        'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa20%20text%20%7B%20fill%3A%23444%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa20%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23666%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22247.3203125%22%20y%3D%22218.3%22%3ESecond%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-        'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa21%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa21%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22277%22%20y%3D%22218.3%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E'
-    ];
     let activeIndex = 0;
     let isOpen = false;
-
     let toggle;
 </script>
 
 <Navbar color="dark" theme="dark">
-    <NavbarBrand href="/" class="me-auto">sveltestrap</NavbarBrand>
+    <NavbarBrand href="/" class="me-auto">Scorion</NavbarBrand>
     <NavbarToggler on:click={toggle} class="me-2" />
     <Collapse {isOpen} navbar>
         <Nav navbar>
@@ -36,11 +29,8 @@
     </Collapse>
 </Navbar>
 
-
 <main class="container">
     <div class="flex">
-
-
         <div class="section recordings">
             <h2 style="font-size: 22px">Recordings</h2>
             <div class="recording">
@@ -66,9 +56,30 @@
 
         <div class="section texts">
             <h2 style="font-size: 22px">Texts</h2>
-            <div class="text-item">Feedback 23 11-1</div>
-            <div class="text-item">Feedback 23 11-2</div>
-            <div class="text-item">Feedback 24 11-1</div>
+            <div class="text-item">
+                <div class="text-item-name">
+                    Feedback 23 11-1
+                </div>
+                <div class="text-item-date" style="font-size: 12px; font-style: italic">
+                    29-07-2014
+                </div>
+            </div>
+            <div class="text-item">
+                <div class="text-item-name" >
+                    Feedback 23 11-1
+                </div>
+                <div class="text-item-date" style="font-size: 12px; font-style: italic">
+                    29-07-2014
+                </div>
+            </div>
+            <div class="text-item">
+                <div class="text-item-name">
+                    Feedback 23 11-1
+                </div>
+                <div class="text-item-date" style="font-size: 12px; font-style: italic">
+                    29-07-2014
+                </div>
+            </div>
         </div>
     </div>
 
@@ -80,9 +91,6 @@
         <p>Excellent job, but blablabla</p>
     </Card>
 
-
-
-
     <div class="record-send">
         <section class="record-container">
             <button class="record-btn">🔴</button>
@@ -92,29 +100,12 @@
     </div>
 
 
-
     <div class="input-section">
         <input type="text" placeholder="Type feedback here..." />
         <button class="send-btn">Send</button>
     </div>
 
-    <Carousel {items} bind:activeIndex>
-        <div class="carousel-inner">
-            {#each items as item, index}
-                <CarouselItem bind:activeIndex itemIndex={index}>
-                    <img src={item} class="d-block w-100" alt="{item} {index + 1}" />
-                </CarouselItem>
-            {/each}
-        </div>
-
-        <CarouselControl direction="prev" bind:activeIndex {items} />
-        <CarouselControl direction="next" bind:activeIndex {items} />
-    </Carousel>
-
 </main>
-
-
-
 
 <style>
 
@@ -135,9 +126,13 @@
         border: 1px solid var(--clr-light);
     }
 
+    .recording:hover{
+        border-color: blueviolet;
+    }
 
-
-
+    .text-item:hover{
+        border-color: blueviolet;
+    }
 
     .recordings, .texts {
         display: flex;
@@ -172,16 +167,16 @@
         transform: scale(1.1);
     }
 
-
-
-
     .text-item {
         display: flex;
         align-items: center;
         padding: 0.5rem;
         border: 1px solid var(--clr-light);
         cursor: pointer;
+        justify-content: space-between;
     }
+
+
 
     .text-item.selected {
         border: 1px solid var(--clr-purple);
@@ -199,17 +194,11 @@
         font-style: oblique;
     }
 
-
-
-
-
-
     .record-send {
         display: flex;
         align-items: center;
         gap: 1rem;
         margin-top: 150px;
-
     }
 
     .record-container{
